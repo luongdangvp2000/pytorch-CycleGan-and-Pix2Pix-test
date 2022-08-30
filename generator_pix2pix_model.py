70e9146c197e3dd3b0f9f9f43de6fb5cb414cd7e
luongdangvp2000 <luongdangvp2000@gmail.com>
2022-08-30 09:49:52 +0000

    fix sths

--- generator_pix2pix_model.py
+++ generator_pix2pix_model.py
@@ -1,2 +1,11 @@
 import torch
 import torch.nn as nn
+
+class Block(nn.Module):
+    def __init__(self, in_channels, out_channels, down=True, act="relu", use_dropout=False):
+        super().__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, 4, 2, 1, bias=False, padding_mode="reflect"),
+            if down
+            else nn.ConvTranspose2d()
+        )
